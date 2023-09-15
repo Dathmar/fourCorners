a5b54a078fcb4c0803af88fc2101c8ac4cf4657c
asher <asher.danner@gmail.com>
2023-09-15 11:01:51 -0500

    Allow blank fields for preforma-quotes support.

--- items/models.py
+++ items/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Item(models.Model):
     quantity = models.IntegerField()
     description = models.CharField(max_length=255, blank=True, null=True)
-    weight = models.DecimalField(max_digits=5, decimal_places=2)
-    length = models.DecimalField(max_digits=5, decimal_places=2)
-    width = models.DecimalField(max_digits=5, decimal_places=2)
-    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    length = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    width = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    height = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     value = models.DecimalField(max_digits=15, decimal_places=2, default=None, null=True)
 
     date_created = models.DateTimeField(auto_now_add=True)
